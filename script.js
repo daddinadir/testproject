1691a8432830052546ee897130af42d224e308a3
NADIR <NADIR>
2017-02-23 01:02:22 +0100

    add some files
    add gitignore & script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var f1 = function(){
+	return 14;
+},f2 = function(){
+	return 20;
+},f3 = function(){
+	return 15;
+}
+module.exports.object = {
+	func1:f1,
+	func2:f2,
+	func3:f3,
+	val1:12,
+	val2:"str",
+	val3:true
+}
--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-console.log("hello world")
+var s = require('./script')
+
+var i = 12+s.object.func1()
+console.log("hello world: "+i)
